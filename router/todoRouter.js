daef4f1c29d728e7afb3a5366b0cc7b082526751
crowwan <crowwan@naver.com>
2023-03-13 19:42:23 +0900

    feat: todo get routing done

--- router/todoRouter.js
+++ router/todoRouter.js
@@ -1,4 +1,17 @@
 const express = require("express");
+const supabase = require("../model/supabaseClient");
 const router = express.Router();
 
-router.get("/", (req, res) => {});
+const TABLE = "todos";
+
+router.get("/", async (req, res) => {
+  const uid = req.cookies?.userId;
+  console.log(uid);
+  if (!uid) return res.status(401).send("Not Authorized");
+
+  const { data, error } = await supabase.from(TABLE).select("*").eq("uid", uid);
+
+  return res.status(200).send(data);
+});
+
+module.exports = router;
