ffed4582caf930a2dd5446d4da06a0f8a849de2e
crowwan <crowwan@naver.com>
2023-03-13 20:08:53 +0900

    feat: update router controller done, change status code

--- router/todoRouter.js
+++ router/todoRouter.js
@@ -23,14 +23,22 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/:id", async (req, res) => {
+  const { id } = req.params;
   const body = req.body;
   const { error } = await supabase.from(TABLE).update(body).eq("id", id);
   if (error) {
     return res.status(500).send("Cannot update data from db");
   }
-  return res.status(200).send("ok");
+  return res.status(201).send("ok");
 });
 
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { error } = await supabase.from(TABLE).delete().eq("id", id);
+  if (error) {
+    return res.status(500).send("Cannot delete data from db");
+  }
+  return res.status(204).send("ok");
+});
 
 module.exports = router;
