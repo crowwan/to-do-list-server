4f04033d8d5438586b88f71d9942fab0d626324d
crowwan <crowwan@naver.com>
2023-03-13 20:33:09 +0900

    feat: add createTodo router, extract controllers

--- router/todoRouter.js
+++ router/todoRouter.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const supabase = require("../model/supabaseClient");
 const router = express.Router();
-
-const TABLE = "todos";
+const {
+  getTodos,
+  updateTodos,
+  deleteTodos,
+  createTodos,
+} = require("../controllers/todoControllers");
 
 router.use((req, res, next) => {
   const uid = req.cookies?.userId;
@@ -11,34 +14,12 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get("/", async (req, res) => {
-  const { data, error } = await supabase
-    .from(TABLE)
-    .select("*")
-    .eq("uid", req.uid);
-  if (error) {
-    return res.status(500).send("Cannot get data from db");
-  }
-  return res.status(200).send(data);
-});
+router.get("/", getTodos);
 
-router.post("/:id", async (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  const { error } = await supabase.from(TABLE).update(body).eq("id", id);
-  if (error) {
-    return res.status(500).send("Cannot update data from db");
-  }
-  return res.status(201).send("ok");
-});
+router.post("/", createTodos);
 
-router.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  const { error } = await supabase.from(TABLE).delete().eq("id", id);
-  if (error) {
-    return res.status(500).send("Cannot delete data from db");
-  }
-  return res.status(204).send("ok");
-});
+router.post("/:id", updateTodos);
+
+router.delete("/:id", deleteTodos);
 
 module.exports = router;
