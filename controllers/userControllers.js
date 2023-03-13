afa72e80d4c9cd56d4dcba7106e9d0fa9abfd408
crowwan <crowwan@naver.com>
2023-03-13 20:39:21 +0900

    feat: extract user controllers

---
+++ controllers/userControllers.js
@@ -0,0 +1,66 @@
+const supabase = require("../model/supabaseClient");
+const crypto = require("crypto");
+
+const TABLE = "users";
+
+const makeHashed = (a) => crypto.createHash("sha256").update(a).digest("hex");
+
+const cookieOptions = {
+  domain: "localhost:3000",
+  path: "/",
+  httpOnly: true,
+};
+
+module.exports = {
+  logIn: async (req, res) => {
+    const { data, error } = await supabase.from(TABLE).select("*");
+    if (error) {
+      return res.status(500).send("Cannot get data from db");
+    }
+    const { uid, password } = req.body;
+    const hashedPassword = makeHashed(password);
+    const user = {
+      ...data.find((a) => a.uid === uid && a.password === hashedPassword),
+    };
+    if (!user) return res.status(401).send("user not found");
+
+    res.cookie("userId", uid, cookieOptions);
+    res.redirect("/user/userinfo");
+  },
+  signUp: async (req, res) => {
+    const { uid, password } = req.body;
+    const hashedPassword = makeHashed(password);
+
+    const { data, error } = await supabase
+      .from(TABLE)
+      .insert({ uid, password: hashedPassword })
+      .select();
+
+    if (error) {
+      return res.status(500).send("Cannot create user to db");
+    }
+
+    res.cookie("userId", data[0].uid, cookieOptions);
+    res.redirect("/userinfo");
+  },
+  logOut: (req, res) => {
+    console.log("test");
+    res.clearCookie("userId", cookieOptions);
+    res.status(205).send("cookie deleted - log out");
+  },
+  getUserInfo: async (req, res) => {
+    const userId = req.cookies?.userId;
+    const { data, error } = await supabase.from("users").select("*");
+    const user = { ...data.find((a) => a.uid === userId) };
+
+    if (!userId || !user.uid) {
+      return res.status(401).send("Not Authorized");
+    } else {
+      delete user.password;
+      res.status(200).json({
+        success: true,
+        data: user.uid,
+      });
+    }
+  },
+};
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,74 +1,21 @@
 const express = require("express");
-const supabase = require("../model/supabaseClient");
-const crypto = require("crypto");
+const {
+  logIn,
+  getUserInfo,
+  signUp,
+  logOut,
+} = require("../controllers/userControllers");
 const router = express.Router();
-
-const TABLE = "users";
-
-const makeHashed = (a) => crypto.createHash("sha256").update(a).digest("hex");
-
-const cookieOptions = {
-  domain: "localhost:3000",
-  path: "/",
-  httpOnly: true,
-};
 
 router.get("/", (req, res) => {
   res.send("user");
 });
-router.post("/login", async (req, res) => {
-  const { data, error } = await supabase.from(TABLE).select("*");
-  if (error) {
-    return res.status(500).send("Cannot get data from db");
-  }
-  const { uid, password } = req.body;
-  const hashedPassword = makeHashed(password);
-  const user = {
-    ...data.find((a) => a.uid === uid && a.password === hashedPassword),
-  };
-  if (!user) return res.status(401).send("user not found");
+router.post("/login", logIn);
 
-  res.cookie("userId", uid, cookieOptions);
-  res.redirect("/user/userinfo");
-});
+router.get("/userinfo", getUserInfo);
 
-router.get("/userinfo", async (req, res) => {
-  const userId = req.cookies?.userId;
-  const { data, error } = await supabase.from("users").select("*");
-  const user = { ...data.find((a) => a.uid === userId) };
+router.post("/signup", signUp);
 
-  if (!userId || !user.uid) {
-    return res.status(401).send("Not Authorized");
-  } else {
-    delete user.password;
-    res.status(200).json({
-      success: true,
-      data: user.uid,
-    });
-  }
-});
-
-router.post("/signup", async (req, res) => {
-  const { uid, password } = req.body;
-  const hashedPassword = makeHashed(password);
-
-  const { data, error } = await supabase
-    .from(TABLE)
-    .insert({ uid, password: hashedPassword })
-    .select();
-
-  if (error) {
-    return res.status(500).send("Cannot create user to db");
-  }
-
-  res.cookie("userId", data[0].uid, cookieOptions);
-  res.redirect("/userinfo");
-});
-
-router.post("/logout", (req, res) => {
-  console.log("test");
-  res.clearCookie("userId", cookieOptions);
-  res.status(205).send("cookie deleted - log out");
-});
+router.post("/logout", logOut);
 
 module.exports = router;
