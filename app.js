0edf7561ad3dde39ce5b7c4f4df25e54472904b3
crowwan <crowwan@naver.com>
2023-04-07 15:13:49 +0900

    feat: FIX corsoptions

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const corsOptions = {
   origin: "https://d28pa5c8zqo8q9.cloudfront.net",
   credentials: true,
   methods: ["GET", "POST", "OPTIONS", "DELETE", "PUT"],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: "Content-Type,Authorization",
 };
 const app = express();
 
