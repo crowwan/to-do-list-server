3471fdc97927ac0214e6e124452227a4876208e9
crowwan <crowwan@naver.com>
2023-04-07 10:48:50 +0900

    feat: FIX cookie options

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -9,8 +9,6 @@ const cookieOptions = {
   domain: "ec2-54-180-116-211.ap-northeast-2.compute.amazonaws.com",
   path: "/",
   httpOnly: true,
-  sameSite: "none",
-  secure: true,
 };
 
 module.exports = {
