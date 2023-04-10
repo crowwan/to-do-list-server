aa52cd6eeba962d184f1e87ebeba7ed177fe23a9
crowwan <crowwan@naver.com>
2023-04-10 16:04:53 +0900

    feat: MOVE react app to server folder

--- app.js
+++ app.js
@@ -2,29 +2,31 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
+const path = require("path");
 require("dotenv").config();
 
-const corsOptions = {
-  origin: "https://master--precious-sawine-ce36fa.netlify.app",
-  credentials: true,
-  methods: ["GET", "POST", "OPTIONS", "DELETE", "PUT"],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: "Content-Type,Authorization,Cookie",
-};
+// const corsOptions = {
+//   origin: "https://master--precious-sawine-ce36fa.netlify.app",
+//   credentials: true,
+//   methods: ["GET", "POST", "OPTIONS", "DELETE", "PUT"],
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+//   allowedHeaders: "Content-Type,Authorization,Cookie",
+// };
 const app = express();
 
 const userRouter = require("./router/userRouter");
 const todoRouter = require("./router/todoRouter");
 
 app.use(morgan("tiny"));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "to-do-list/build")));
 
-app.get("/", (req, res) => {
-  res.send("ok");
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/to-do-list/build/index.html"));
 });
 
 app.use("/user", userRouter);
